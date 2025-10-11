bf9c903160fec763e87bec5be953b09c8d558c30
Amit Singh <cse.amitallenhouse@gmail.com>
2025-10-11 19:36:33 +0530

    how to pass parameters to the apex class method using wire decorator

--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -1,9 +1,29 @@
 import { LightningElement, wire } from 'lwc';
 import getAllProducts from '@salesforce/apex/ProductController.getProducts'
+import getProductsByProductCode from '@salesforce/apex/ProductController.getProductsByProductCode';
+
 export default class ProductList extends LightningElement {
+
+    filteredProducts;
+    errors;
+    productCode = 'GC1060';
 
     @wire(getAllProducts) products;
     // { data, error }
     // products.data = List of Products
     // products.error;
+
+    @wire(getProductsByProductCode,{
+        productCode: "$productCode",
+        productFamily: 'Simple'
+    })
+    wiredFilteredProducts({ data, error }){
+        if(data){
+            this.filteredProducts = data;
+            console.log(this.filteredProducts);
+        }else if(error){
+            this.errors = error;
+            console.error(this.errors);
+        }
+    }
 }
